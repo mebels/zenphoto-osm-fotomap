e2350f4f55ff7f7ad6d0256b0a4c1c5ef40df9a1
mebels <36734239+mebels@users.noreply.github.com>
2018-03-20 15:43:19 +0100

    Create albummap.php
    This is only a content for a albumxxx.php in a zenphoto theme. Please read the README.

---
+++ albummap.php
@@ -0,0 +1,45 @@
+<?php
+$images = array();
+$result = query("SELECT `images`.`albumid`, `images`.`id` AS imageid, `images`.`EXIFGPSLatitude` AS exiflat, `images`.`EXIFGPSLongitude` AS exiflon, `images`.`filename`, `albums`.`id`, `albums`.`folder` FROM `images` INNER JOIN `albums` ON `images`.`albumid`=`albums`.`id` WHERE `images`.`show` = '1'");
+
+while ($row = db_fetch_assoc($result)) {
+
+if (isset($row["exiflat"]) && isset($row["exiflon"])) {
+
+$imageid = $row["imageid"];
+$filename = $row["filename"];
+$folder = $row["folder"];
+
+$images[$imageid] = array (
+"filename" => $filename,
+"folder" => $folder
+);
+} // if lat,lon
+
+} // while
+
+$geodata = array();
+$geodata_i = 0;
+foreach ($images as $imagesarray) {
+
+$albumobject = newAlbum($imagesarray["folder"]);
+$imageobject = newImage($albumobject, $imagesarray["filename"]);
+
+$image_exif_lat = $imageobject->get('EXIFGPSLatitude');
+$image_exif_lon = $imageobject->get('EXIFGPSLongitude');
+$image_exif_thumb = "<a href='" . $imageobject->getLink() . "'><img src='" . $imageobject->getThumb() . "'></a>";
+
+$geodata[$geodata_i] = array(
+'lat' => $image_exif_lat,
+'long' => $image_exif_lon,
+'title' => '',
+'desc' => '',
+'thumb' => $image_exif_thumb,
+'current' => '0'
+);
+
+$geodata_i++;
+} // foreach
+
+printOpenStreetMap($geodata);
+?>
